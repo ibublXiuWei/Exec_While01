f6f6419ef16cd3668c3101f3138f5bbe030e306c
xiuwei <xiuwei@DESKTOP-CIM4AC6>
2022-10-22 02:12:33 -0700

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int N = 1;
+			while ((1 + N) * N / 2 < 105)
+			{
+				N += 1;
+			}
+			Console.WriteLine(N);
+		}
+	}
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int i = new int();
+			while (i <= 300)
+			{
+				i += 1;
+				var root = 300 % i;
+				if (root == 0)
+				{
+					Console.WriteLine(i);
+				}
+			}
+		}
+	}
+}
